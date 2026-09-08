7a0641a2f2db8b41ede14810974eff5454cfa11a
Andres Mendez <andrescmendeza@gmail.com>
2026-09-08 10:01:27 -0500

    update multa table structure and add config_facultad_multas table; enhance laboratoristas table with toggle functionality

--- sql-scripts/db_structure.sql
+++ sql-scripts/db_structure.sql
@@ -301,4 +301,27 @@ COMMENT ON COLUMN milab.multa.laboratorista_documento_id IS 'Referencia a milab.
 COMMENT ON COLUMN milab.multa.usuario_sancionado_id IS 'Referencia a milab.usuario.id';
 COMMENT ON COLUMN milab.multa.ual_id IS 'Referencia a milab.ual.ual_id';
 
+ALTER TABLE multa
+ALTER COLUMN tipo_sancion TYPE CHARACTER VARYING(500);
+
+ALTER TABLE multa
+ALTER COLUMN cat_multa TYPE CHARACTER VARYING(500);
+
+CREATE TABLE IF NOT EXISTS config_facultad_multas (
+    facultad_id INTEGER NOT NULL PRIMARY KEY,
+    permite_crear_multas_activas_directas BOOLEAN NOT NULL DEFAULT FALSE,
+    permite_saldar_multas_directas BOOLEAN NOT NULL DEFAULT FALSE,
+    fecha_ultima_modificacion TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    documento_ultimo_autorizador TEXT,
+    accion_ultima TEXT DEFAULT 'inicial',
+    CONSTRAINT config_facultad_multas_facultad_fk
+        FOREIGN KEY (facultad_id) REFERENCES milab.facultad(facultad_id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO config_facultad_multas (facultad_id)
+SELECT facultad_id
+FROM milab.facultad
+ON CONFLICT (facultad_id) DO NOTHING;
+
 COMMIT;
