a371caf9b018cc5aca391a86b42634b7476b761f
Andres Mendez <andrescmendeza@gmail.com>
2026-09-16 10:44:03 -0500

    Fix consulta UALs

--- src/routes/middlewares/auth.js
+++ src/routes/middlewares/auth.js
@@ -5,7 +5,6 @@ function renderAuthError(res, overrides = {}) {
     message: '¡Algo ha salido mal!',
     message2: 'Inténtalo nuevamente',
     limit: 'noSession',
-    adminErrorDetail: null,
     ...overrides,
   };
 
